d92edc20b7e033f0cb6c32c5d43dd8a2b32c7ea1
KonstantinMerkel <konstantin.merkel@tutanota.de>
2025-01-06 16:38:48 +0100

    .

--- tictactoe.py
+++ tictactoe.py
@@ -1,135 +1,107 @@
-class tictactoe:
-
-    gameover = False
-    game = [[None,None,None], [None,None,None], [None,None,None]]
-    move = 'x'
-    moved = 0
-    won_by = None
-
+class TicTacToe:
     def __init__(self):
+        self.game = [[None, None, None], [None, None, None], [None, None, None]]
+        self.move = 'x'
+        self.gameover = False
+        self.moved = 0
+        self.won_by = None
         print('Willkommen im Spiel')
 
     def make_move(self, x, y):
-        game = self.game
-        move = self.move
-
-        if game[x][y] != None:  
-            print('illegal move')
+        if self.game[x][y] is not None:
+            print('Illegal move')
             return
-        game[x][y] = move
+
+        self.game[x][y] = self.move
         self.print_game()
         self.moved += 1
         self.gameover = self.winner()
+
         if self.gameover:
-            self.won_by = move
-        else: 
+            self.won_by = self.move
+            print(f'{self.move} has won the game')
+        else:
             self.gameover = self.stale_game()
 
-        if move == 'x':
-            self.move = 'o'
-        else:
-            self.move='x'
+        # Switch player
+        self.move = 'o' if self.move == 'x' else 'x'
 
-    
     def winner(self):
-        game = self.game
-
+        # Check rows and columns
         for x in range(3):
-            if game[x] == ['x','x','x'] or game[x] == ['o','o','o']:
+            if self.game[x] == ['x', 'x', 'x'] or self.game[x] == ['o', 'o', 'o']:
                 return True
-            elif game[0][x] == game[1][x] and game[1][x] == game[2][x] and game[2][x] != None:
+            elif self.game[0][x] == self.game[1][x] == self.game[2][x] and self.game[2][x] is not None:
                 return True
-        if game[0][0] == game[1][1] and game[1][1] == game[2][2] and game[2][2] != None:
+
+        # Check diagonals
+        if self.game[0][0] == self.game[1][1] == self.game[2][2] and self.game[2][2] is not None:
+            return True
+        if self.game[0][2] == self.game[1][1] == self.game[2][0] and self.game[2][0] is not None:
             return True
 
-        if game[0][2] == game[1][1] and game[1][1] == game[2][0]and game[2][0] != None:
-            print('winner')
-            return True
-
+        return False
 
     def stale_game(self):
-        game = self.game
-        list3 = [
-         [game[0][0], game[0][1], game[0][2]],
-         [game[1][0], game[1][1], game[1][2]],
-         [game[2][0], game[2][1], game[2][2]],
-         [game[0][0], game[1][0], game[2][0]],
-         [game[0][1], game[1][1], game[2][1]],
-         [game[0][2], game[1][2], game[2][2]],
-         [game[0][0], game[1][1], game[2][2]],
-         [game[0][2], game[1][1], game[2][0]]
-        ]
-        for element in list3:
-            if not 'x' in element or not 'o' in element:
+        # Check for a stale game (no winner and no empty spaces left)
+        for row in self.game:
+            if None in row:
                 return False
-        print('unentschieden niemand kann mehr gewinnen')
+        print('Unentschieden ')
         return True
 
-
-
     def print_game(self):
-        game = self.game
-        move = self.move
-        for x in range(3):
-            one = game[x][0]
-            two = game[x][1]
-            three = game[x][2]
-            print(f'\t {one} \t | \t {two} \t | \t {three}')
+        for row in self.game:
+            print(f"\t {' \t | '.join([str(cell) if cell else ' ' for cell in row])} \t")
         print()
-        
-class point_counter:
 
-    counter = [0,0]
-    
+class PointCounter:
     def __init__(self):
-        counter = self.counter
-    
-    def addwin(self, winner):
+        self.counter = {'x': 0, 'o': 0}
 
-        counter = self.counter
+    def add_win(self, winner):
+        if winner in self.counter:
+            self.counter[winner] += 1
 
-        if winner == 'x':
-            counter[0] += 1
-        elif winner == 'o':
-            counter[1] += 1
-    
-    def print_gamecount(self):
-        counter = self.counter
-        print(f"x hat {counter[0]} mal gewonnen; o hat {counter[1]} mal gewonnen\n")
+    def print_game_count(self):
+        print(f"x hat {self.counter['x']} mal gewonnen; o hat {self.counter['o']} mal gewonnen\n")
 
-
-instance = tictactoe()
-counter_instance = point_counter()
+# Main game loop
+instance = TicTacToe()
+counter_instance = PointCounter()
 first_move = 'x'
-print('gebe mit 1-3 die Zeile an und dann mit 1-3 die Spalte \n (1|1) | (1|2) | (1|3) \n (2|1) | (2|2) | (2|3) \n (3|1) | (3|2) | (3|3)')
+
+print('Gebe mit 1-3 die Zeile an und dann mit 1-3 die Spalte an:')
+print("(1|1) | (1|2) | (1|3)\n(2|1) | (2|2) | (2|3)\n(3|1) | (3|2) | (3|3)")
+
 while True:
+    while not instance.gameover:
+        try:
+            x = int(input(f"{instance.move.upper()}: Gebe Zeile (1-3): ")) - 1
+            y = int(input(f"{instance.move.upper()}: Gebe Spalte (1-3): ")) - 1
+            if x < 0 or x > 2 or y < 0 or y > 2:
+                print("Ungültige Eingabe. Wählen Sie eine Zahl zwischen 1 und 3.")
+                continue
+            instance.make_move(x, y)
+        except ValueError:
+            print("Ungültige Eingabe. Geben Sie bitte eine Zahl ein.")
+    
+    # After game is over
+    if instance.won_by:
+        counter_instance.add_win(instance.won_by)
 
-    while(not instance.gameover):
-        x = int(input('x: ')) - 1
-        y = int(input('y: ')) - 1
-        instance.make_move(x, y)
-
-    rematch = input('Erneut spielen? (y/n)')
-
-    if rematch.upper() =='N':
+    rematch = input('Erneut spielen? (y/n): ').strip().lower()
+    if rematch == 'n':
         break
-    else:
-        if instance.won_by == 'x':
-            counter_instance.addwin('x')
-        if instance.won_by == 'o':
-            counter_instance.addwin('o')
-        
-        counter_instance.print_gamecount()
-        print
 
-        instance.game = [[None,None,None], [None,None,None], [None,None,None]]
-        instance.gameover = False
-        instance.moved = 0
-        instance.won_by = None
-        if first_move == 'x':
-            instance.move = 'o' 
-            first_move = 'o'
-        elif first_move == 'o':
-            instance.move = 'x' 
-            first_move = 'x'
-        print(f"Die neue Runde wird von {first_move} begonnen\n")
+    counter_instance.print_game_count()
+
+    # Reset game for rematch
+    instance.game = [[None, None, None], [None, None, None], [None, None, None]]
+    instance.gameover = False
+    instance.moved = 0
+    instance.won_by = None
+    instance.move = 'o' if first_move == 'x' else 'x'
+    first_move = instance.move
+
+    print(f"Die neue Runde wird von {first_move.upper()} begonnen\n")
